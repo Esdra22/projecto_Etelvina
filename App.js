5301a397e7dfdad8c4b166ab2b886c03f474ad59
Esdra22 <esdraandre@hotmail.com>
2021-06-15 11:32:55 +0100

    Update App.js
    Actualizado

--- App.js
+++ App.js
@@ -1,109 +1,316 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Box} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-import styled from 'styled-components/native';
+import { Feather, MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-   
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <>
+      <View style={styles.header}>
+        <View style={styles.firstView}>
+          <Text>
+            <Feather name="map-pin" color="#fff" size={20} />
+          </Text>
 
-      <View >
-        <Text style={styles.HeaderTitle }>
-         Multicaixa
-         </Text>
-         <Text style={styles.Text1}> mais próximo de ti...</Text>
+          <View style={styles.TextStyles}>
+            <Text style={{ color: "#fff", fontWeight: "bold"}}>
+              Multicaixa
+            </Text>
+            <Text style={{ color: "#fff" }}>Mais próximo de ti...</Text>
+          </View>
+
+          <Text>
+            <Feather name="info" color="#fff" size={20} />
+          </Text>
+        </View>
+
+        <View style={styles.secondView}>
+          <Feather name="send" color="#fff" size={18} />
+          <TextInput
+            placeholder="Insira o endereço"
+            style={styles.Input}
+            placeholderTextColor="#fff"
+          />
+
+          <Feather name="search" color="#fff" size={18} />
+        </View>
       </View>
-    <View>
-    <TextInput
-        Placeholder="insira o seu endereço"
-        placeholderTextColor="#000"
-         style= {styles.TextInput}
-          /> 
-    </View>
 
+      <View style={styles.MainContainer}>
+        <View style={styles.Card}>
+          <Text
+            style={{
+              marginTop: 15,
+              marginBottom: 10,
+              fontWeight: "bold",
+              fontSize: 16,
+              marginLeft: 15,
+            }}
+          >
+            ATM - Banco BPC
+          </Text>
 
-<View  style={styles.caixa} >
-<Text  style={styles.textView1}> ATM - BANCO BPC </Text>
+          <View style={styles.CarContent}>
+            <View style={styles.FirsColumm}>
+              <View style={styles.FirstContent}>
+                <Feather name="wifi" color="#555" />
+                <Text style={styles.FirstText}>Sistemas</Text>
+              </View>
 
+              <View style={styles.FirstContent}>
+                <Feather name="users" color="#555" />
+                <Text style={styles.FirstText}>Enchetes</Text>
+              </View>
 
-</View>
-<View  style={styles.caixa} >
-<Text  style={styles.textView1}> ATM - BANCO BAI </Text>
-</View>
-<View  style={styles.caixa} >
-<Text  style={styles.textView1}> ATM - BANCO BIC </Text>
-</View>
+              <View style={styles.FirstContent}>
+                <Feather name="send" color="#555" />
+                <Text style={styles.FirstText}>Avenida 21 de Janeiro</Text>
+              </View>
+            </View>
 
+            <View style={styles.SecondColumn}>
+              <View style={styles.FirsColumm}>
+                <View style={styles.SecondContent}>
+                  <MaterialIcons name="money" color="#555" />
+                  <Text style={styles.FirstText}>Dinheiro</Text>
+                </View>
 
-</View>
- 
- 
+                <View style={styles.SecondContent}>
+                  <Feather name="file" color="#555" />
+                  <Text style={styles.FirstText}>Papel</Text>
+                </View>
 
+                <View style={styles.SecondContent}>
+                  <Text style={styles.FirstText}>
+                    ~826 m | <Feather name="map-pin" color="#555" />
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.footer}>
+            <MaterialIcons name="directions" />
+            <Text> Direção</Text>
+          </View>
+        </View>
+
+        <View style={styles.Card}>
+          <Text
+            style={{
+              marginTop: 15,
+              marginBottom: 10,
+              fontWeight: "bold",
+              fontSize: 16,
+              marginLeft: 15,
+            }}
+          >
+            ATM - Banco BFA
+          </Text>
+
+          <View style={styles.CarContent}>
+            <View style={styles.FirsColumm}>
+              <View style={styles.FirstContent}>
+                <Feather name="wifi" color="#555" />
+                <Text style={styles.FirstText}>Sistemas</Text>
+              </View>
+
+              <View style={styles.FirstContent}>
+                <Feather name="users" color="#555" />
+                <Text style={styles.FirstText}>Enchetes</Text>
+              </View>
+
+              <View style={styles.FirstContent}>
+                <Feather name="send" color="#555" />
+                <Text style={styles.FirstText}>Avenida 21 de Janeiro</Text>
+              </View>
+            </View>
+
+            <View style={styles.SecondColumn}>
+              <View style={styles.FirsColumm}>
+                <View style={styles.SecondContent}>
+                  <MaterialIcons name="money" color="#555" />
+                  <Text style={styles.FirstText}>Dinheiro</Text>
+                </View>
+
+                <View style={styles.SecondContent}>
+                  <Feather name="file" color="#555" />
+                  <Text style={styles.FirstText}>Papel</Text>
+                </View>
+
+                <View style={styles.SecondContent}>
+                  <Text style={styles.FirstText}>
+                    ~826 m | <Feather name="map-pin" color="#555" />
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.footer}>
+            <MaterialIcons name="directions" />
+            <Text> Direção</Text>
+          </View>
+        </View>
+
+        <View style={styles.Card}>
+          <Text
+            style={{
+              marginTop: 15,
+              marginBottom: 10,
+              fontWeight: "bold",
+              fontSize: 16,
+              marginLeft: 15,
+            }}
+          >
+            ATM - Banco Prestigio
+          </Text>
+
+          <View style={styles.CarContent}>
+            <View style={styles.FirsColumm}>
+              <View style={styles.FirstContent}>
+                <Feather name="wifi" color="#555" />
+                <Text style={styles.FirstText}>Sistemas</Text>
+              </View>
+
+              <View style={styles.FirstContent}>
+                <Feather name="users" color="#555" />
+                <Text style={styles.FirstText}>Enchetes</Text>
+              </View>
+
+              <View style={styles.FirstContent}>
+                <Feather name="send" color="#555" />
+                <Text style={styles.FirstText}>Avenida 21 de Janeiro</Text>
+              </View>
+            </View>
+
+            <View style={styles.SecondColumn}>
+              <View style={styles.FirsColumm}>
+                <View style={styles.SecondContent}>
+                  <MaterialIcons name="money" color="#555" />
+                  <Text style={styles.FirstText}>Dinheiro</Text>
+                </View>
+
+                <View style={styles.SecondContent}>
+                  <Feather name="file" color="#555" />
+                  <Text style={styles.FirstText}>Papel</Text>
+                </View>
+
+                <View style={styles.SecondContent}>
+                  <Text style={styles.FirstText}>
+                    ~826 m | <Feather name="map-pin" color="#555" />
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.footer}>
+            <MaterialIcons name="directions" />
+            <Text> Direção</Text>
+          </View>
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFA500'
-   
-  },
-  Text1:{
-    fontSize:16,
-    color:'#FFF'
-
+  header: {
+    width: "100%",
+    height: 170,
+    backgroundColor: "orange",
   },
 
-  HeaderTitle:{
-    marginTop: -150,
-    width: 250,
-    fontSize:24,
-    fontWeight:'bold',
-    color:'#FFF'
-
+  TextStyles: {
+    color: "#fff",
+    flexDirection: "column",
+    textAlign: "center",
   },
-    TextInput:{
 
-    marginTop: -90,
-    fontSize:12,
-    color: '#000',
-    backgroundColor:"#ffff",
-    paddingRight: 40,
-    width: 300,
-    height: 40,
-    paddingVertical:10,
-    borderRadius:3
-  
-  },
-  caixa:{
-    marginTop:8,
-    justifyContent: "space-between",
+  firstView: {
+    width: "100%",
+    backgroundColor: "orange",
     flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    height: 120,
     alignItems: "center",
-    width: 300,
-    height: 110,
-    backgroundColor:"#FFF",
-    borderRadius:10
-
   },
-  textView1:{
-    marginTop: -60,
+
+  secondView: {
+    width: "90%",
+    backgroundColor: "darkorange",
+    marginTop: -18,
+    marginLeft: 20,
+    flexDirection: "row",
+    height: 50,
     alignItems: "center",
-    fontWeight:'bold',
+    borderRadius: 6,
+    paddingHorizontal: 10,
   },
-  
-  textView3:{
-    marginTop:-3,
-    paddingRight:70,
-   fontSize:12,
 
-  }
-  
-  
- 
+  Input: {
+    flex: 1,
+    marginLeft: 10,
+    backgroundColor: "transparent",
+  },
+
+  MainContainer: {
+    backgroundColor: "#ddd",
+    height: "100%",
+  },
+
+  Card: {
+    backgroundColor: "#fff",
+    width: "90%",
+    height: 170,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
+
+  FirsColumm: {
+    width: "50%",
+  },
+
+  SecondColumn: {
+    width: "50%",
+  },
+
+  CarContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  FirstContent: {
+    flexDirection: "row",
+    marginTop: 6,
+    paddingLeft: 40,
+  },
+
+  SecondContent: {
+    flexDirection: "row",
+    marginTop: 6,
+    paddingLeft: 10,
+  },
+
+  FirstText: {
+    fontSize: 12,
+    marginLeft: 6,
+    color: "#555",
+  },
+
+  footer: {
+    borderTopWidth: 1,
+    borderTopColor: "#ddd",
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingTop: 6,
+  },
 });
